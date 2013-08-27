783e9d8d450796af9d0208fb4e35ad232252f6a2
TisKarl <thoren.gbg.karl@gmail.com>
2013-08-27 12:57:03 +0200

    First commit

---
+++ src/Test.java
@@ -0,0 +1,21 @@
+
+public class Test {
+	public static void main(String[] args) {
+		//Deklararion och initiering
+		int number = 5;
+		double decNumber = 2.43;
+		char sign = 'a';
+		boolean answer = false;
+		String text = "Hello";
+		
+		//tilldelningar
+		number = 7;
+		decNumber = 1.1;
+		sign = 'b';
+		answer = true;
+		text = "World";
+	}
+}
+
+
+
---
+++ src/Test2.java
@@ -0,0 +1,13 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		char test = 500 + 500;
+		System.out.println(test);
+		
+		
+		
+		
+	}
+
+}
